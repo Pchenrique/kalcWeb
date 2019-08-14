bdc8daa8367bbd3c6742e6c44c14c52160eade5b
Pchenrique <pchenrique1998@hotmail.com>
2019-08-14 17:12:21 -0300

    Criação das funções para os calculos

--- js/kalkWeb.js
+++ js/kalkWeb.js
@@ -1,56 +1,145 @@
+//Variaveis de operação 
 var soma = '+';
-var multiplicacao = '*';
-var divisao = '/';
+var multiplicacao = 'x';
+var divisao = '÷';
 var subtracao = '-';
 var igualdade = '=';
-var atual=0 ;
-var resultado;
-var antigo;
+//variaveis ajustes dos valores
+var operando=false;
+var operadorAtual = "";
+var operadorAntigo = "";
+
+var resultado = 0;
+var antigo = 0;
+var atual = 0;
+var temPonto = false;
 
 const screen = document.querySelector(".screen");
 
-// const botoes = document.querySelectorAll("numbers button");
-
-// for(let x=0; x<botoes.length;x++){
-// 	botoes[x].onclick = apresentar(botoes[x].innerText);
-// }
-
- const pegarValor = function(valor){
+const pegarValor = function(valor){
  	apresentar(valor);
- } 
+} 
 
-const apresentar = function(valor){
-	if(valor == "." && screen.innerText == "0"){
-		screen.innerText = "0";
-		screen.innerText += valor;
-	}else if(screen.innerText == "0"){
-		screen.innerText = "";
-		screen.innerText += valor;
-	}else{
-		screen.innerText += valor;
-	}
-	
+const reset = function(){
+	screen.innerText = "0";
+	atual= 0 ;
+	resultado= 0;
+	antigo= 0;
+	operando = 0;
+	operadorAntigo = "";
+	operadorAtual = "";
+	temPonto = false;
+	console.clear();
 }
 
-const apagar = function(){
-	screen.innerText = "0";
-	atual=0 ;
-	resultado=0;
-	antigo=0;
+const apresentar = function(valor){
+	//console.log(operando);
+	if(operando){
+		operando = false;
+		
+		if(valor == "."){
+			screen.innerText = "0";
+			screen.innerText += valor;
+			temPonto = true;
+		}else{
+			screen.innerText = "";
+			screen.innerText += valor;
+		}
+	}else{
+		if(valor == "." && screen.innerText == "0"){
+				screen.innerText = "0";
+				screen.innerText += valor;
+				temPonto = true;
+		// }else if(temPonto && valor == "."){
+		// 	return;
+		}else if(screen.innerText == "0"){
+			screen.innerText = "";
+			screen.innerText += valor;
+		}else{
+			if(valor == "." && screen.innerText != "0" && !temPonto){
+				screen.innerText += valor;
+				temPonto = true;
+			}else if(temPonto && valor == "."){
+				return;
+			}else{
+				screen.innerText += valor;
+			}
+		}
+	}
+}
+
+const calcular = function(valorAntigo, operador, valorAtual){
+	if(operador == soma){
+		operadorAntigo = "";
+		return antigo+atual;
+	}else if(operador == subtracao){
+		operadorAntigo = "";
+		return antigo-atual;
+	}else if(operador == divisao){
+		operadorAntigo = "";
+		return antigo/atual;
+	}else if(operador == multiplicacao){
+		operadorAntigo = "";
+		return antigo*atual;
+	}
+}
+
+//resolver o problema da igualdade
+const igual = function(){
+	// operando = true;
+	// atual = Number(screen.innerText);
+	// if(operadorAtual == ""){
+	// 	apresentar(atual);
+	// }else{
+	// 	operador(operadorAtual);	
+	// }
+	// var r = {antigo, atual, operadorAntigo, operadorAtual, resultado};
+	// console.table(r);
+	
+	operando = true;
+	atual = Number(screen.innerText);
+
+	if(operadorAtual == ""){
+		//atual = atual + atual;
+		apresentar(atual);
+	}else{
+		if(operadorAntigo == ""){
+			resultado = calcular(antigo, operadorAtual, atual);
+			antigo = resultado;
+			apresentar(resultado);
+			operadorAtual = "";
+		}else{
+			resultado = calcular(antigo, operadorAntigo, atual);
+			antigo = resultado;
+			apresentar(resultado);
+			operadorAtual = "";
+		}
+	}
+	var r = {antigo, atual, operadorAntigo, operadorAtual, resultado};
+	console.table(r);
 }
 
 const operador = function(operador){
-	if(operador == soma){
-		antigo = atual;
-		atual = parseInt(screen.innerText);
-		screen.innerText = "";
-		resultado = somar(antigo, atual);
-		apresentar(resultado);
-		//console.log(atual);
-	}else if(operador == '='){
 
+	// console.log(operador);
+	atual = Number(screen.innerText);
+	operadorAntigo = operadorAtual;
+	if(operadorAntigo == ""){
+		antigo = atual;
 	}
-}
-const somar = function(a, b=0){
-	return a+b;
+	operadorAtual = operador;
+	operando = true;
+
+	if(operadorAntigo != ""){
+			resultado = calcular(antigo, operadorAntigo, atual);
+			antigo = resultado;
+			apresentar(resultado);
+			operando = true;
+	}
+
+	var t = {antigo, atual, operadorAntigo, operadorAtual, resultado};
+	console.table(t);
+
+	return;
+
 }
